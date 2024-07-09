04860f18cda87478b235a544ba75fb942e6262b5
Anpatil1 <nangareavadhut@gmail.com>
2024-07-09 19:00:13 +0530

    Resolved merge conflicts.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-        <h3>Designed and Developed by </h3><a    href="https://github.com/Anpatil1" >Avadhut Nangare-Patil </a>
+        <h3>Designed and Developed by<a    href="https://github.com/Anpatil1" >Avadhut Nangare-Patil </a> </h3>
           {/* <h3>Designed and Developed by Avadhut Nangare-Patil</h3> */}
         </Col>
         <Col md="4" className="footer-copywright">
